1c7befcc76ab4ce6731cdb1a3306255428486738
Dan Gayle <dang@spokesman.com>
2017-01-02 13:25:33 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+require('tachyons/css/tachyons.min.css')
+
+
+const App = () => (
+  <div className="mw9 center">
+    <h2 className="red sans-serif tc">Hello, world</h2>
+  </div>
+);
+
+export default App;
